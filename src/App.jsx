9c3e9310941ce15ed94e450ead71fd9934cb660d
vikaskrishna-vk <vikasgalgali6@gmail.com>
2026-03-25 20:09:32 +0530

    verify'

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import AdminDashboard from "./pages/AdminDashboard";
 import AuctionPage from "./pages/AuctionPage";
 import FertilizerMarket from "./pages/FertilizerMarket";
 import Register from "./pages/Register";
-import VerifyOTP from "./pages/VerifyOTP";
+// import VerifyOTP from "./pages/VerifyOTP";
 import KisanSahayak from "./pages/KisanSahayak"; 
 import CreateAuction from "./pages/CreateAuction"
 import MarketPrice from "./pages/MarketPrice"
@@ -38,7 +38,7 @@ function App() {
         <Route path="/auction" element={<AuctionPage />} />
         <Route path="/fertilizers" element={<FertilizerMarket />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/verify-otp" element={<VerifyOTP />} />
+        {/* <Route path="/verify-otp" element={<VerifyOTP />} /> */}
         <Route path="/farmer" element={<FarmerDashboard />} />
         <Route path="/buyer" element={<BuyerDashboard />} />
         <Route path="/dealer" element={<DealerDashboard />} />
